dc7455fc4497e0e2fb145fff83c6bedf8f6403ff
Paul Pound <paul.pound@gmail.com>
2017-11-08 13:49:37 -0400

    now storing tokens in ORCID ds with an xacml policy to limit access to this ds also started work on orcid logout

---
+++ js/upei_roblib_orcid.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Javascript functions
+ */
+
+Drupal.behaviors.upei_roblib_orcid = {
+  attach: function (context, settings) {
+    /*
+     *  If the Drupal logout button/url has an id of orcid-and-drupal-logout we
+     *  will try to log you out of ORCID as well as Drupal.  Only required
+     *  in a situation where computer access is shared with several users and
+     *  browser cache is not cleared between users.
+     */
+    jQuery("#orcid-and-drupal-logout").click(function () {
+      var orcid_logout_url = Drupal.settings.upei_roblib_orcid.orcid_logout_url;
+      jQuery.getJSON(orcid_logout_url, function(data) {
+        jQuery.each(data, function(key, val) {
+          alert(key + " -- " + val);
+        })
+          console.log("Attempted to log out of ORCID. Still Logged In: " + data.loggedIn);
+      });
+    })
+  }
+}
